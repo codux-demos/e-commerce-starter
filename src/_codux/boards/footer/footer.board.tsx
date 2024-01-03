8e469d8525661d435a97e2b47fbde540c8dd6ad0
Vladyslav Goloshchapov <vladyslavg@wix.com>
2024-01-03 19:21:27 +0200

    verify that common styling works

--- src/_codux/boards/footer/footer.board.tsx
+++ src/_codux/boards/footer/footer.board.tsx
@@ -1,10 +1,11 @@
 import { createBoard } from '@wixc3/react-board';
 import { Footer } from '../../../components/footer/footer';
+import Theme_module from '../../../styles/theme.module.scss';
 
 export default createBoard({
     name: 'Footer',
     Board: () => (
-        <div style={{ width: '100vw' }}>
+        <div style={{ width: '100vw' }} className={Theme_module.common}>
             <Footer />
         </div>
     ),
