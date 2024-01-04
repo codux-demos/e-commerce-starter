2718e577b537c91cfde5c23216bab1bcfcded6b7
Arnon Kehat <arnonk@wix.com>
2024-01-04 14:37:48 +0200

    made some improvements to my boards and components

--- src/components/gallery-card/gallery-card.tsx
+++ src/components/gallery-card/gallery-card.tsx
@@ -15,11 +15,11 @@ export interface GalleryCardProps {
 
 export const GalleryCard = ({ className, price, currency, description, rating, raters }: GalleryCardProps) => {
     return <div className={classNames(styles.root, className)}>
-        <CardImage />
+        <CardImage imageSrc="https://static.wixstatic.com/media/c22c23_77c2b89535374768bec1d6337dcdbddc~mv2.jpg/v1/fill/w_625,h_625,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/c22c23_77c2b89535374768bec1d6337dcdbddc~mv2.jpg" />
         <div className={styles['card-content']}>
             <p className={styles['item-description']}>{description}</p>
             <p className={styles['item-price']}>{currency}{price}</p>
-            <StarRating rating={rating} raters={raters}/>
+            <StarRating rating={rating} raters={raters} />
         </div>
     </div>;
 };
