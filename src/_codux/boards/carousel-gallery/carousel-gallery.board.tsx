30901caef6997b167b59ec23f60e4e2b40157887
Arnon Kehat <arnonk@wix.com>
2024-01-04 17:24:54 +0200

    continue work on carousel layout

--- src/_codux/boards/carousel-gallery/carousel-gallery.board.tsx
+++ src/_codux/boards/carousel-gallery/carousel-gallery.board.tsx
@@ -46,6 +46,9 @@ export default createBoard({
     ),
     isSnippet: true,
     environmentProps: {
-        windowWidth: 1662
+        canvasMargin: {
+            top: 0, right: 0, bottom: 0, left: 0
+        },
+        windowWidth: 692
     }
 });
--- src/components/carousel-gallery/carousel-gallery.tsx
+++ src/components/carousel-gallery/carousel-gallery.tsx
@@ -14,22 +14,20 @@ export const CarouselGallery = ({ items, className }: CarouselGalleryProps) => {
         <div>
             <h1>Heading 1</h1>
         </div>
-        <div className={styles['items-container']}>
+        <div className={styles.carousel}>
             <button>Button</button>
-
-            {items.map(item => (
-                <GalleryCard
-                    description={item.description}
-                    currency={item.currency}
-                    price={item.price}
-                    rating={item.rating}
-                    raters={item.raters}
-                    className={styles['gallery-card']}
-                />
-            ))}
+            <div className={styles['items-container']}>
+                {items.map(item => (
+                    <GalleryCard
+                        description={item.description}
+                        currency={item.currency}
+                        price={item.price}
+                        rating={item.rating}
+                        raters={item.raters}
+                        className={styles['gallery-card']}
+                    />
+                ))}</div>
             <button>Button</button>
-
-
         </div>
         <div>
             <button>Button</button>
