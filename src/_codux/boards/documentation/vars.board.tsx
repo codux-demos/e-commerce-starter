e3a69320eb5a129125e982028d4be6b6c7450292
Dana-Levy <66260729+Dana-Levy@users.noreply.github.com>
2024-01-07 11:37:34 +0200

    Dana/vars (#17)
    * Add text vars
    
    * fix large and extra large text
    
    ---------
    
    Co-authored-by: Codux <codux@codux.com>

--- src/_codux/boards/documentation/vars.board.tsx
+++ src/_codux/boards/documentation/vars.board.tsx
@@ -6,9 +6,18 @@ export default createBoard({
     name: 'Vars',
     Board: () => (
         <div className={boardStyle.root}>
-            <p className={Vars_module.small}>using small-font var</p>
-            <p className={Vars_module.large}>using large-font var</p>
+            <h1 className={Vars_module.extraLargeTitle}>Extra Large Title</h1>
+            <h2 className={Vars_module.largeTitle}>Large Title</h2>
+            <h3 className={Vars_module.mediumTitle}>Medium Title</h3>
+            <h4 className={Vars_module.smallTitle}>Small Title</h4>
+            <p className={Vars_module.extraLargeText}>Extra Large Text</p>
+            <p className={Vars_module.largeText}>Large Text</p>
+            <p className={Vars_module.MediumText}>Small Text</p>
+            <p className={Vars_module.extraSmallText}>Extra Small Text</p>
         </div>
     ),
     isSnippet: true,
+    environmentProps: {
+        windowWidth: 1024
+    }
 });
