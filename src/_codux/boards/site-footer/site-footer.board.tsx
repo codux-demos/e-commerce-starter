9733aaa06287b6b7197256e3fa33cb302e83c775
Codux <codux@codux.com>
2024-01-07 12:42:20 +0200

    Footer structure

--- src/_codux/boards/site-footer/site-footer.board.tsx
+++ src/_codux/boards/site-footer/site-footer.board.tsx
@@ -5,4 +5,9 @@ export default createBoard({
     name: 'SiteFooter',
     Board: () => <SiteFooter />,
     isSnippet: true,
+    environmentProps: {
+        canvasWidth: 1190,
+        windowWidth: 1024,
+        windowHeight: 768
+    }
 });
--- src/components/site-footer/site-footer.tsx
+++ src/components/site-footer/site-footer.tsx
@@ -7,7 +7,20 @@ export interface SiteFooterProps {
 
 export const SiteFooter = ({ className }: SiteFooterProps) => {
     return <div className={classNames(styles.root, className)}>
-        <p className={styles.footerTitle}>Store Location</p>
-        <p className={styles.footerContent}>500 Terry Francine Street San Francisco, CA 94158 info@mysite.com</p>
-    </div>;
+        <div>
+            <div>
+                <p className={styles.footerTitle}>Store Location</p>
+                <p className={styles.footerContent}>500 Terry Francine Street San Francisco, CA 94158 info@mysite.com</p></div>
+            <div>
+                <p className={styles.footerContent}>123-456-7890</p>
+            </div></div>
+        <div>
+            <p className={styles.footerTitle}>Shop</p>
+            <a href="/https://www.wix.com/website-template/view/html/2860?originUrl=https%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates%2Fhtml%2Fonline-store%2Felectronics&amp;tpClick=view_button&amp;esi=3f77d9ac-c430-4c6f-b713-1218dfcd14a8" className={styles.footerContent}>Shop All</a></div>
+        <div>
+            <p className={styles.footerTitle}>Store Location</p>
+            <p className={styles.footerContent}>500 Terry Francine Street San Francisco, CA 94158 info@mysite.com</p></div>
+        <div>
+            <p className={styles.footerTitle}>Store Location</p>
+            <p className={styles.footerContent}>500 Terry Francine Street San Francisco, CA 94158 info@mysite.com</p></div></div>;
 };
