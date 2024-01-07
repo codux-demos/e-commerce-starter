a33bc4710f7d9cff0b4d1e7f4f25302b98575492
Codux <codux@codux.com>
2024-01-07 14:08:40 +0200

    Fix footer structure

--- src/_codux/boards/site-footer/site-footer.board.tsx
+++ src/_codux/boards/site-footer/site-footer.board.tsx
@@ -6,8 +6,9 @@ export default createBoard({
     Board: () => <SiteFooter />,
     isSnippet: true,
     environmentProps: {
-        canvasWidth: 1190,
-        windowWidth: 1024,
-        windowHeight: 768
+        canvasWidth: 1334,
+        windowWidth: 1460,
+        windowHeight: 768,
+        canvasHeight: 428,
     }
 });
--- src/components/site-footer/site-footer.tsx
+++ src/components/site-footer/site-footer.tsx
@@ -7,20 +7,47 @@ export interface SiteFooterProps {
 
 export const SiteFooter = ({ className }: SiteFooterProps) => {
     return <div className={classNames(styles.root, className)}>
-        <div>
-            <div>
+        <div className={styles.footerColumn}>
+            <div className={styles.footerTitle}>
                 <p className={styles.footerTitle}>Store Location</p>
                 <p className={styles.footerContent}>500 Terry Francine Street San Francisco, CA 94158 info@mysite.com</p></div>
-            <div>
+            <div className={styles.footerContent}>
                 <p className={styles.footerContent}>123-456-7890</p>
             </div></div>
-        <div>
-            <p className={styles.footerTitle}>Shop</p>
-            <a href="/https://www.wix.com/website-template/view/html/2860?originUrl=https%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates%2Fhtml%2Fonline-store%2Felectronics&amp;tpClick=view_button&amp;esi=3f77d9ac-c430-4c6f-b713-1218dfcd14a8" className={styles.footerContent}>Shop All</a></div>
-        <div>
-            <p className={styles.footerTitle}>Store Location</p>
-            <p className={styles.footerContent}>500 Terry Francine Street San Francisco, CA 94158 info@mysite.com</p></div>
-        <div>
-            <p className={styles.footerTitle}>Store Location</p>
-            <p className={styles.footerContent}>500 Terry Francine Street San Francisco, CA 94158 info@mysite.com</p></div></div>;
+        <div className={styles.footerColumn}>
+            <div className={styles.footerTitle}>
+                <p>Shop</p>
+            </div>
+            <div className={styles.footerContent}>
+                <a href="/https://www.wix.com/website-template/view/html/2860?originUrl=https%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates%2Fhtml%2Fonline-store%2Felectronics&amp;tpClick=view_button&amp;esi=3f77d9ac-c430-4c6f-b713-1218dfcd14a8" className={styles.footerContent}>Shop All</a>
+                <a href="/https://www.wix.com/website-template/view/html/2860?originUrl=https%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates%2Fhtml%2Fonline-store%2Felectronics&amp;tpClick=view_button&amp;esi=3f77d9ac-c430-4c6f-b713-1218dfcd14a8" className={styles.footerContent}>Computers</a>
+                <a href="/https://www.wix.com/website-template/view/html/2860?originUrl=https%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates%2Fhtml%2Fonline-store%2Felectronics&amp;tpClick=view_button&amp;esi=3f77d9ac-c430-4c6f-b713-1218dfcd14a8" className={styles.footerContent}>Tablets</a>
+                <a href="/https://www.wix.com/website-template/view/html/2860?originUrl=https%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates%2Fhtml%2Fonline-store%2Felectronics&amp;tpClick=view_button&amp;esi=3f77d9ac-c430-4c6f-b713-1218dfcd14a8" className={styles.footerContent}>Drones &amp; Cameras</a>
+                <a href="/https://www.wix.com/website-template/view/html/2860?originUrl=https%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates%2Fhtml%2Fonline-store%2Felectronics&amp;tpClick=view_button&amp;esi=3f77d9ac-c430-4c6f-b713-1218dfcd14a8" className={styles.footerContent}>Audio</a>
+                <a href="/https://www.wix.com/website-template/view/html/2860?originUrl=https%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates%2Fhtml%2Fonline-store%2Felectronics&amp;tpClick=view_button&amp;esi=3f77d9ac-c430-4c6f-b713-1218dfcd14a8" className={styles.footerContent}>Mobile</a>
+                <a href="/https://www.wix.com/website-template/view/html/2860?originUrl=https%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates%2Fhtml%2Fonline-store%2Felectronics&amp;tpClick=view_button&amp;esi=3f77d9ac-c430-4c6f-b713-1218dfcd14a8" className={styles.footerContent}>T.V &amp; Home Cineme</a>
+                <a href="/https://www.wix.com/website-template/view/html/2860?originUrl=https%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates%2Fhtml%2Fonline-store%2Felectronics&amp;tpClick=view_button&amp;esi=3f77d9ac-c430-4c6f-b713-1218dfcd14a8" className={styles.footerContent}>Wearable Tech</a>
+                <a href="/https://www.wix.com/website-template/view/html/2860?originUrl=https%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates%2Fhtml%2Fonline-store%2Felectronics&amp;tpClick=view_button&amp;esi=3f77d9ac-c430-4c6f-b713-1218dfcd14a8" className={styles.footerContent}>Sale</a>
+            </div>
+        </div>
+        <div className={styles.footerColumn}>
+            <div className={styles.footerTitle}>
+                <p>Customer Support</p>
+            </div>
+            <div className={styles.footerContent}>
+                <a href="/https://www.wix.com/website-template/view/html/2860?originUrl=https%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates%2Fhtml%2Fonline-store%2Felectronics&amp;tpClick=view_button&amp;esi=3f77d9ac-c430-4c6f-b713-1218dfcd14a8" className={styles.footerContent}>Contact Us</a>
+                <a href="/https://www.wix.com/website-template/view/html/2860?originUrl=https%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates%2Fhtml%2Fonline-store%2Felectronics&amp;tpClick=view_button&amp;esi=3f77d9ac-c430-4c6f-b713-1218dfcd14a8" className={styles.footerContent}>Help  Center</a>
+                <a href="/https://www.wix.com/website-template/view/html/2860?originUrl=https%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates%2Fhtml%2Fonline-store%2Felectronics&amp;tpClick=view_button&amp;esi=3f77d9ac-c430-4c6f-b713-1218dfcd14a8" className={styles.footerContent}>About Us</a>
+                <a href="/https://www.wix.com/website-template/view/html/2860?originUrl=https%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates%2Fhtml%2Fonline-store%2Felectronics&amp;tpClick=view_button&amp;esi=3f77d9ac-c430-4c6f-b713-1218dfcd14a8" className={styles.footerContent}>Career</a></div>
+        </div>
+        <div className={styles.footerColumn}>
+            <div className={styles.footerTitle}>
+                <p>Policy</p>
+            </div>
+            <div className={styles.footerContent}>
+                <a href="/https://www.wix.com/website-template/view/html/2860?originUrl=https%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates%2Fhtml%2Fonline-store%2Felectronics&amp;tpClick=view_button&amp;esi=3f77d9ac-c430-4c6f-b713-1218dfcd14a8" className={styles.footerContent}>Shipping &amp; Return</a>
+                <a href="/https://www.wix.com/website-template/view/html/2860?originUrl=https%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates%2Fhtml%2Fonline-store%2Felectronics&amp;tpClick=view_button&amp;esi=3f77d9ac-c430-4c6f-b713-1218dfcd14a8" className={styles.footerContent}>Terms &amp; Condition</a>
+                <a href="/https://www.wix.com/website-template/view/html/2860?originUrl=https%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates%2Fhtml%2Fonline-store%2Felectronics&amp;tpClick=view_button&amp;esi=3f77d9ac-c430-4c6f-b713-1218dfcd14a8" className={styles.footerContent}>Payment Methods</a>
+                <a href="/https://www.wix.com/website-template/view/html/2860?originUrl=https%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates%2Fhtml%2Fonline-store%2Felectronics&amp;tpClick=view_button&amp;esi=3f77d9ac-c430-4c6f-b713-1218dfcd14a8" className={styles.footerContent}>FAQ</a></div>
+        </div></div>;
 };
