14f050a4e6f0b0a5ba40dd4220fce7b8aa7a800e
Codux <codux@codux.com>
2024-01-07 14:28:41 +0200

    Removed width and height

--- src/_codux/boards/site-footer/site-footer.board.tsx
+++ src/_codux/boards/site-footer/site-footer.board.tsx
@@ -6,10 +6,7 @@ export default createBoard({
     Board: () => <SiteFooter />,
     isSnippet: true,
     environmentProps: {
-        canvasWidth: 1334,
-        windowWidth: 1920,
-        windowHeight: 1080,
-        canvasHeight: 428,
+        canvasWidth: 1334, canvasHeight: 428,
         canvasMargin: {
             top: 0, right: 0, bottom: 0, left: 0
         }
