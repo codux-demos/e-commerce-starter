ea91614ddb7f477ed89967a7ef237f4b13e022e7
Arnon Kehat <arnonk@wix.com>
2024-01-08 15:26:05 +0200

    minor changes to code formatting

--- src/_codux/boards/hero-image/hero-image.board.tsx
+++ src/_codux/boards/hero-image/hero-image.board.tsx
@@ -3,6 +3,13 @@ import { HeroImage } from '../../../components/hero-image/hero-image';
 
 export default createBoard({
     name: 'HeroImage',
-    Board: () => <HeroImage bottomLabel="Selected Smartphone Brands" buttonLabel="Shop" title="Up to 30% off" topLabel="Holiday Deals" />,
+    Board: () => (
+        <HeroImage
+            bottomLabel="Selected Smartphone Brands"
+            buttonLabel="Shop"
+            title="Up to 30% off"
+            topLabel="Holiday Deals"
+        />
+    ),
     isSnippet: true,
 });
--- src/components/hero-image/hero-image.tsx
+++ src/components/hero-image/hero-image.tsx
@@ -9,19 +9,38 @@ export interface HeroImageProps {
     buttonLabel: string;
 }
 
-
-
-export const HeroImage = ({ title, bottomLabel, buttonLabel, topLabel, className }: HeroImageProps) => {
-    return <div className={classNames(styles.root, className)}>
-        <div className={styles['image-container']}>
-            <img src="https://static.wixstatic.com/media/c22c23_e140bfa8cd6f4cb2ac5ee6e204f64073~mv2.jpg/v1/fill/w_571,h_468,al_t,q_80,usm_0.66_1.00_0.01,enc_auto/c22c23_e140bfa8cd6f4cb2ac5ee6e204f64073~mv2.jpg" alt="" className={styles.image} />
-            <div className={styles.overlay}>
-                <div>
-                    <p className={styles['overlay-text']}>{topLabel}</p>
-                    <h1 className={classNames(styles['overlay-text'], styles['overlay-header'])}>{title}</h1>
-                    <p className={styles['overlay-text']}>{bottomLabel}</p>
-                    <button className={classNames(styles.secondaryButton, styles['overlay-button'])}>{buttonLabel}</button>
+export const HeroImage = ({
+    title,
+    bottomLabel,
+    buttonLabel,
+    topLabel,
+    className,
+}: HeroImageProps) => {
+    return (
+        <div className={classNames(styles.root, className)}>
+            <div className={styles['image-container']}>
+                <img
+                    src="https://static.wixstatic.com/media/c22c23_e140bfa8cd6f4cb2ac5ee6e204f64073~mv2.jpg/v1/fill/w_571,h_468,al_t,q_80,usm_0.66_1.00_0.01,enc_auto/c22c23_e140bfa8cd6f4cb2ac5ee6e204f64073~mv2.jpg"
+                    alt=""
+                    className={styles.image}
+                />
+                <div className={styles.overlay}>
+                    <div>
+                        <p className={styles['overlay-text']}>{topLabel}</p>
+                        <h1
+                            className={classNames(styles['overlay-text'], styles['overlay-header'])}
+                        >
+                            {title}
+                        </h1>
+                        <p className={styles['overlay-text']}>{bottomLabel}</p>
+                        <button
+                            className={classNames(styles.secondaryButton, styles['overlay-button'])}
+                        >
+                            {buttonLabel}
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div></div>;
+        </div>
+    );
 };
