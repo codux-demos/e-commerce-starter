fdb4e31092fdc020994a620c882756c3b5e16d8c
Dana-Levy <66260729+Dana-Levy@users.noreply.github.com>
2024-01-08 17:32:11 +0200

    Dana/vars (#26)
    * Add text vars
    
    * fix large and extra large text
    
    * Add colors
    
    
    ---------
    
    Co-authored-by: Codux <codux@codux.com>
    Co-authored-by: alissawix <alissav@wix.com>

--- src/_codux/boards/documentation/vars.board.tsx
+++ src/_codux/boards/documentation/vars.board.tsx
@@ -1,26 +1,53 @@
 import { createBoard } from '@wixc3/react-board';
 import Vars_module from './vars.module.scss';
 import boardStyle from './documentation.module.scss';
+import Classnames from 'classnames';
 
 export default createBoard({
     name: 'Vars',
     Board: () => (
         <div className={boardStyle.root}>
+            <div className={Vars_module.colors}>
+                <div>
+                    <div className={Classnames(Vars_module.circle, Vars_module.white)}></div>
+                    <p className={Vars_module.circleTitle}>White</p>
+                </div>
+                <div>
+                    <div className={Classnames(Vars_module.circle, Vars_module.purple)}></div>
+                    <p className={Vars_module.circleTitle}>Purple</p>
+                </div>
+                <div>
+                    <div className={Classnames(Vars_module.circle, Vars_module.black)}></div>
+                    <p className={Vars_module.circleTitle}>Black</p>
+                </div>
+                <div>
+                    <div className={Classnames(Vars_module.circle, Vars_module.gray)}></div>
+                    <p className={Vars_module.circleTitle}>Gray</p>
+                </div>
+                <div>
+                    <div className={Classnames(Vars_module.circle, Vars_module.lightGray)}></div>
+                    <p className={Vars_module.circleTitle}>Light Gray</p>
+                </div>
+            </div>
             <h1 className={Vars_module.extraLargeTitle}>Extra Large Title</h1>
-            <h2 className={Vars_module.largeTitle}>Large Title 60px</h2>
-            <h3 className={Vars_module.mediumTitle}>Medium Title 50px</h3>
-            <h4 className={Vars_module.smallTitle}>Small Title 30px</h4>
-            <p className={Vars_module.extraLargeText}>Extra Large Text 25px</p>
-            <p className={Vars_module.largeText}>Large Text 20px</p>
-            <p className={Vars_module.largeTextBold}>Large Text 17px</p>
-            <p className={Vars_module.MediumText}>Medium Text 17px</p>
-            <p className={Vars_module.smallText}>Small text 16px</p>
-            <p className={Vars_module.extraSmallText}>Extra Small Text 14px</p>
+            <h2 className={Vars_module.largeTitle}>Large Title</h2>
+            <h3 className={Vars_module.mediumTitle}>Medium Title</h3>
+            <h4 className={Vars_module.smallTitle}>Small Title</h4>
+            <p className={Vars_module.extraLargeText}>Extra Large Text</p>
+            <p className={Vars_module.largeText}>Large Text</p>
+            <p className={Vars_module.MediumText}>Small Text</p>
+            <p className={Vars_module.extraSmallText}>Extra Small Text</p>
         </div>
     ),
-    isSnippet: true,
+    isSnippet: false,
     environmentProps: {
         windowWidth: 1024,
-        windowHeight: 758
-    }
+        canvasMargin: {
+            top: 0,
+            right: 0,
+            bottom: 0,
+            left: 0,
+        },
+        windowHeight: 746,
+    },
 });
