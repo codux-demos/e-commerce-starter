88d29254392c0157185a698c791e2d8990eaf011
Tetiana Mandziuk <tetianaman@wix.com>
2024-01-08 14:41:59 +0100

    data type fix

--- src/_codux/fakeData/fakeData.ts
+++ src/_codux/fakeData/fakeData.ts
@@ -1,5 +1,6 @@
 import { products } from '@wix/stores';
 import { faker } from '@faker-js/faker';
+import { GetProductResponseNonNullableFields } from '@wix/stores/build/cjs/src/stores-catalog-v1-product.universal';
 
 export function createProducts(numOfItems?: number) {
     return Array.from(new Array(numOfItems || 10)).map(createProduct);
@@ -7,22 +8,35 @@ export function createProducts(numOfItems?: number) {
 
 export function createProduct(
     id?: string
-): products.Product & { rating?: number; ratingCount?: number } {
+): products.Product & GetProductResponseNonNullableFields['product'] {
     return {
         _id: id ?? faker.string.uuid(),
         slug: faker.lorem.word(),
         name: faker.commerce.productName(),
         description: faker.commerce.productDescription(),
         media: {
-            mainMedia: {
-                image: {
-                    url: faker.image.urlLoremFlickr({
-                        category: 'product',
-                        height: 241,
-                        width: 241,
-                    }),
+            items: [
+                {
+                    image: {
+                        url: faker.image.urlLoremFlickr({
+                            category: 'product',
+                            height: 241,
+                            width: 241,
+                        }),
+                    },
+                    title: faker.lorem.word(),
                 },
-            },
+                {
+                    image: {
+                        url: faker.image.urlLoremFlickr({
+                            category: 'product',
+                            height: 241,
+                            width: 241,
+                        }),
+                    },
+                    title: faker.lorem.word(),
+                },
+            ],
         },
         price: {
             formatted: {
@@ -31,7 +45,5 @@ export function createProduct(
                 }),
             },
         },
-        rating: faker.number.float({ min: 0, max: 5, precision: 0.1 }),
-        ratingCount: faker.number.int({ min: 0, max: 1000 }),
-    };
+    } as products.Product & GetProductResponseNonNullableFields['product'];
 }
