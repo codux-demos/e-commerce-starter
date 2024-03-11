cd9d6a1fc4b5ea69c4fc955515df2b23af31aca5
ShireeWill <shiree@wix.com>
2024-03-11 17:04:29 +0200

    cart is empty design fix

--- src/components/cart/cart-drawer.tsx
+++ src/components/cart/cart-drawer.tsx
@@ -2,6 +2,8 @@ import { CartItem } from './cart-item/cart-item';
 import commonStyles from '@styles/common-styles.module.scss';
 import styles from './cart.module.scss';
 import { useCart, useCartTotals } from '../../api/api-hooks';
+import CartItem_module from './cart-item/cart-item.module.scss';
+import CartDrawer_module from './cart-drawer.module.scss';
 
 export function CartDrawer() {
     const { data: cart } = useCart();
@@ -10,14 +12,10 @@ export function CartDrawer() {
     const isEmpty = !cart?.lineItems || cart.lineItems.length === 0;
 
     return isEmpty ? (
-        <div>Cart is empty</div>
+        <div className={CartDrawer_module.emptyCart}>Cart is empty</div>
     ) : (
         <div className={styles.cart}>
-            <div>
-                {cart?.lineItems?.map((item) => (
-                    <CartItem key={item._id} cartItem={item} />
-                ))}
-            </div>
+            <div>{cart?.lineItems?.map((item) => <CartItem key={item._id} cartItem={item} />)}</div>
             <div>
                 <label>
                     Subtotal: {cartTotals?.priceSummary?.subtotal?.formattedConvertedAmount}
