0a07eb870de5884b08f7f094751daff4b9eee708
mikawix <mikan@wix.com>
2024-03-19 14:48:36 +0200

    replace div to h1 and paragraph

--- src/pages/about-page/about-page.tsx
+++ src/pages/about-page/about-page.tsx
@@ -9,13 +9,13 @@ export const AboutPage = ({ className }: AboutPageProps) => {
     return (
 <div className={classNames(styles.root, className)}>
             <div className={styles.text}>
-                <div className={styles.title}>I&apos;M A TITLE</div>
-                <div className={styles.paragraph}>
+                <h1 className={styles.title}>I&apos;M A TITLE</h1>
+                <p className={styles.paragraph}>
                     I’m a paragraph. Add your own text and edit me on properties panel on the right.
                     It’s easy. Just select me and make changes to the font on the styles panel on
                     the right. I’m a great place for you to tell a story and let your viewers know a
                     little more about you.
-                </div>
+                </p>
             </div>
             <img
                 src="https://images.unsplash.com/photo-1622542796254-5b9c46ab0d2f?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3Dwixplosives.github.io/codux-assets-storage/add-panel/image-placeholder.jpg"
