6f141358862292b31fffa292941f5d3220d0d12d
Codux <codux@codux.com>
2024-03-19 16:28:45 +0200

    Add title and logo

--- src/_codux/boards/documentation/common-styles.board.tsx
+++ src/_codux/boards/documentation/common-styles.board.tsx
@@ -1,11 +1,23 @@
 import { createBoard } from '@wixc3/react-board';
 import commonStyles from '../../../styles/common-styles.module.scss';
 import boardStyle from './documentation.module.scss';
+import CommonStyles_board_module from './common-styles.board.module.scss';
 
 export default createBoard({
     name: 'Theme Styles',
     Board: () => (
         <div className={boardStyle.root}>
+            <div className={CommonStyles_board_module.styleGuideHeader}>
+                <div>
+                    <h1 className={CommonStyles_board_module.styleGuideTitle}>Style Guide</h1>
+                </div>
+                <div className={CommonStyles_board_module.logoContainer}>
+                    <div className={CommonStyles_board_module.logoContainer}>
+                        <div className={CommonStyles_board_module.IconLogo} />
+                        LOGO
+                    </div>
+                </div>
+            </div>
             <div className={boardStyle.box}>
                 <button className={commonStyles.primaryButton}>Primary Button</button>
                 <button className={commonStyles.secondaryButton}>Secondary Button</button>
