c5d56b5bc9083e1fe1ff1629c8959bf902db7243
alissawix <alissav@wix.com>
2024-03-20 12:50:02 +0200

    add client id to main

--- src/_codux/boards-global-setup.ts
+++ src/_codux/boards-global-setup.ts
@@ -1,1 +1,12 @@
 import '../styles/index.css';
+//@ts-ignore
+import env from 'raw-loader!/.env';
+const lines = env.split('\n');
+lines.forEach((line: string) => {
+    const [key, value] = line.split('=');
+    const trimmedKey = key.trim();
+    if (trimmedKey.startsWith('VITE_')) {
+        process.env[trimmedKey] = value.trim();
+    }
+});
+console.log(process.env);
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -8,3 +8,12 @@ declare module '*.svg' {
     >;
     export default ReactComponent;
 }
+
+interface ImportMetaEnv {
+    readonly VITE_WIX_CLIENT_ID: string;
+    // more env variables...
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
