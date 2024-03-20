f289696d80bc11cc2d4b51d2b69b974547e35fe4
Codux <codux@codux.com>
2024-03-20 14:31:05 +0200

    Added categories to the style guide board

--- src/_codux/boards/documentation/common-styles.board.tsx
+++ src/_codux/boards/documentation/common-styles.board.tsx
@@ -18,6 +18,35 @@ export default createBoard({
                     </div>
                 </div>
             </div>
+            <div className={CommonStyles_board_module.categoryContainer}>
+                <div className={CommonStyles_board_module.categoryTitle}>
+                    <img
+                        src="https://static.wixstatic.com/media/8586f3_fa01751d235246e0951c44812f029653~mv2.png/v1/fill/w_28,h_26,al_c,lg_1,q_85,enc_auto/_.png"
+                        alt=""
+                    />
+                    <h2 className={CommonStyles_board_module.title}>Typography</h2>
+                </div>
+                <p className={CommonStyles_board_module.subtitle}>
+                    Defines the fonts used for headings, body text, labels, etc., including size,
+                    weight, and spacing.
+                </p>
+            </div>
+            <div className={CommonStyles_board_module.subCategortContainer}>
+                <h3 className={CommonStyles_board_module.subCategoryTitle}>Font Family</h3>
+                <div className={CommonStyles_board_module.divider}></div>
+            </div>
+            <div className={CommonStyles_board_module.CardItems}>
+                <div className={CommonStyles_board_module.fontFamilySection}>
+                    <span className={CommonStyles_board_module.fontExample}>Aa</span>
+                    <span className={CommonStyles_board_module.fomtFamilyName}>
+                        &quot;DM Sans&quot;, sans-serif
+                    </span>
+                </div>
+            </div>
+            <div className={CommonStyles_board_module.subCategortContainer}>
+                <h3 className={CommonStyles_board_module.subCategoryTitle}>Main Heading / h1</h3>
+                <div className={CommonStyles_board_module.divider}></div>
+            </div>
             <div className={boardStyle.box}>
                 <button className={commonStyles.primaryButton}>Primary Button</button>
                 <button className={commonStyles.secondaryButton}>Secondary Button</button>
@@ -32,10 +61,11 @@ export default createBoard({
     environmentProps: {
         canvasHeight: 52,
         canvasMargin: {
-            top: 0,
-            left: 0,
-            bottom: 0,
+            top: 1,
             right: 0,
+            bottom: 103,
+            left: 0,
         },
+        windowWidth: 1024,
     },
 });
