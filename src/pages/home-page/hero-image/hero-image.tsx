497498120a1426206970e89edadec7a618075681
mikawix <mikan@wix.com>
2024-03-25 18:38:33 +0200

    fix hero images

--- src/pages/home-page/hero-image/hero-image.tsx
+++ src/pages/home-page/hero-image/hero-image.tsx
@@ -1,8 +1,8 @@
-import classNames from 'classnames';
-import styles from './hero-image.module.scss';
 import commonStyles from '@styles/common-styles.module.scss';
 import CommonStyles_module from '../../../styles/common-styles.module.scss';
 import heroImage from '../../../assets/img/[1024_640]_heroImage04.jpg';
+import classNames from 'classnames';
+import styles from './hero-image.module.scss';
 
 export interface HeroImageProps {
     className?: string;
@@ -15,7 +15,6 @@ export interface HeroImageProps {
     secondaryButtonLabel?: string;
     onSecondaryButtonClick?: () => void;
 }
-
 export const HeroImage = ({
     title,
     bottomLabel,
@@ -30,11 +29,11 @@ export const HeroImage = ({
     return (
         <div className={classNames(styles.root, className)}>
             <div className={styles['image-container']}>
-                <img src={heroImage} className={styles.image} />
+                <div className={styles.image} />
                 <div className={styles.overlay}>
                     <div>
-                        <h1 className={styles['overlay-text']}>{title}</h1>
-                        <p className={styles['overlay-header']}>{bottomLabel}</p>
+                        <p className={styles['small-title']}>{title}</p>
+                        <h1 className={styles['big-title']}>{bottomLabel} </h1>
                         {primaryButtonLabel && (
                             <button
                                 onClick={onPrimaryButtonClick}
