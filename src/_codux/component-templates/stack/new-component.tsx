c93e23da989c0e949ee43c9676338a04de808213
mikawix <mikan@wix.com>
2024-03-30 21:22:06 +0300

    created component templates

---
+++ src/_codux/component-templates/stack/new-component.tsx
@@ -0,0 +1,37 @@
+import classNames from 'classnames';
+import styles from './new-component.module.scss';
+import commonStyles from '@styles/common-styles.module.scss';
+
+export interface NewComponentProps {
+    className?: string;
+}
+
+/**
+ * This component was created using Codux's Default new component template.
+ * To create custom component templates, see https://help.codux.com/kb/en/article/kb16522
+ */
+export const NewComponent = ({ className }: NewComponentProps) => {
+    return (
+        <div className={classNames(className, styles.root)}>
+            <div>
+                <img
+                    src="https://wixmp-b7f7090100b13623109851bc.wixmp.com/layouters-starters/img_01.jpg"
+                    alt=""
+                    className={styles.img}
+                />
+            </div>
+            <div className={styles.section}>
+                <img
+                    src="https://wixmp-b7f7090100b13623109851bc.wixmp.com/layouters-starters/img_02.jpg"
+                    alt=""
+                    className={styles.img}
+                />
+                <img
+                    src="https://wixmp-b7f7090100b13623109851bc.wixmp.com/layouters-starters/img_03.jpg"
+                    alt=""
+                    className={styles.img}
+                />
+            </div>
+        </div>
+    );
+};
