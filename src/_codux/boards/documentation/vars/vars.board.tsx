7f9661a7cc595ed10bdae04f66caea2625cf2a88
ShireeWill <shiree@wix.com>
2024-04-01 17:41:56 +0300

    color fix

--- src/_codux/boards/documentation/vars/vars.board.tsx
+++ src/_codux/boards/documentation/vars/vars.board.tsx
@@ -2,38 +2,30 @@ import { createBoard } from '@wixc3/react-board';
 import styles from './vars.module.scss';
 import cx from 'classnames';
 import documentationStyles from '/src/_codux/boards/documentation/documentation.module.scss';
+import Vars_board_module from './vars.board.module.scss';
 
 export default createBoard({
     name: 'Vars',
     Board: () => (
         <div className={documentationStyles.root}>
             <div className={styles.colors}>
-                <div>
+                <div className={styles.wraper}>
                     <div className={cx(styles.circle, styles.white)}></div>
                     <p className={styles.circleTitle}>White</p>
                 </div>
-                <div>
+                <div className={styles.wraper}>
                     <div className={cx(styles.circle, styles.black)}></div>
                     <p className={styles.circleTitle}>Black</p>
                 </div>
-                <div>
-                    <div className={cx(styles.circle, styles.disabled)}></div>
-                    <p className={styles.circleTitle}>disabled</p>
+                <div className={styles.wraper}>
+                    <div className={cx(styles.circle, styles.charcoalBlack)}></div>
+                    <p className={styles.circleTitle}>Charcoal Black</p>
                 </div>
-                <div>
-                    <div className={cx(styles.circle, styles.dirtyWhite)}></div>
-                    <p className={styles.circleTitle}>Dirty White</p>
+                <div className={styles.wraper}>
+                    <div className={cx(styles.circle, styles.snowWhite)}></div>
+                    <p className={styles.circleTitle}>Snow White</p>
                 </div>
             </div>
-            <p className={styles.smallFont}>Small Font</p>
-            <h1 className={styles.extraLargeTitle}>Extra Large Title</h1>
-            <h2 className={styles.largeTitle}>Large Title</h2>
-            <h3 className={styles.mediumTitle}>Medium Title</h3>
-            <h4 className={styles.smallTitle}>Small Title</h4>
-            <p className={styles.extraLargeText}>Extra Large Text</p>
-            <p className={styles.largeText}>Large Text</p>
-            <p className={styles.MediumText}>Small Text</p>
-            <p className={styles.extraSmallText}>Extra Small Text</p>
         </div>
     ),
     isSnippet: false,
