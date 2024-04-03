d9212f508bd3a2dfc263ae14004b31b7575460be
ShireeWill <shiree@wix.com>
2024-04-03 12:57:28 +0300

    added title

--- src/_codux/boards/documentation/vars/vars.board.tsx
+++ src/_codux/boards/documentation/vars/vars.board.tsx
@@ -7,27 +7,10 @@ export default createBoard({
     name: 'Vars',
     Board: () => (
         <div className={documentationStyles.root}>
-            <div className={styles.colors}>
-                <div className={styles.wraper}>
-                    <div className={cx(styles.circle, styles.white)}></div>
-                    <p className={styles.circleTitle}>White</p>
-                </div>
-                <div className={styles.wraper}>
-                    <div className={cx(styles.circle, styles.black)}></div>
-                    <p className={styles.circleTitle}>Black</p>
-                </div>
-                <div className={styles.wraper}>
-                    <div className={cx(styles.circle, styles.charcoalBlack)}></div>
-                    <p className={styles.circleTitle}>Charcoal Black</p>
-                </div>
-                <div className={styles.wraper}>
-                    <div className={cx(styles.circle, styles.snowWhite)}></div>
-                    <p className={styles.circleTitle}>Snow White</p>
-                </div>
-            </div>
+            <p className={styles.Title}>Project Fonts</p>
             <div className={styles['fonts-layout']}>
                 <div>
-                    <p className={styles.circleTitle}>------ Title styles -----</p>
+                    <p className={styles.miniTitle}>------ Title styles -----</p>
                     <div className={styles['extra-large-thin-title']}>Extra large thin title</div>
                     <div className={styles['extra-large-title']}>Extra large title</div>
                     <div className={styles['large-title']}>Large title</div>
@@ -35,17 +18,36 @@ export default createBoard({
                     <div className={styles['extra-small-title']}>Extra small title</div>
                 </div>
                 <div>
-                    <p className={styles.circleTitle}>------ Paragraph styles -----</p>
+                    <p className={styles.miniTitle}>------ Paragraph styles -----</p>
                     <div className={styles['large-paragraph']}>Large paragraph</div>
                     <div className={styles['paragraph-font']}>Paragraph font</div>
                     <div className={styles['medium-paragraph-bold']}>Medium paragraph bold</div>
                     <div className={styles['small-paragraph-font']}>Small paragraph font</div>
                 </div>
                 <div>
-                    <p className={styles.circleTitle}>------ Running text styles -----</p>
+                    <p className={styles.miniTitle}>------ Running text styles -----</p>
                     <div className={styles['large-text']}>Large text</div>
                     <div className={styles['small-text']}>Small text</div>
                     <div className={styles['extra-small-text']}>Extra small text</div>
+                </div>
+            </div>
+            <p className={styles.Title}>Project Colores</p>
+            <div className={styles.colors}>
+                <div className={styles.wrapper}>
+                    <div className={cx(styles.circle, styles.white)}></div>
+                    <p className={styles.miniTitle}>White</p>
+                </div>
+                <div className={styles.wrapper}>
+                    <div className={cx(styles.circle, styles.snowWhite)}></div>
+                    <p className={styles.miniTitle}>Snow White</p>
+                </div>
+                <div className={styles.wrapper}>
+                    <div className={cx(styles.circle, styles.black)}></div>
+                    <p className={styles.miniTitle}>Black</p>
+                </div>
+                <div className={styles.wrapper}>
+                    <div className={cx(styles.circle, styles.charcoalBlack)}></div>
+                    <p className={styles.miniTitle}>Charcoal Black</p>
                 </div>
             </div>
         </div>
@@ -59,5 +61,6 @@ export default createBoard({
             left: 0,
         },
         windowWidth: 1400,
+        windowHeight: 768,
     },
 });
