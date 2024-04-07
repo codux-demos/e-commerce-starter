3ea0f7a6884c595c43cfa538ce1cf00c78e7f1ff
ShireeWill <shiree@wix.com>
2024-04-07 16:44:11 +0300

    added div

--- src/_codux/boards/intro/intro.board.tsx
+++ src/_codux/boards/intro/intro.board.tsx
@@ -6,12 +6,14 @@ export default createBoard({
     name: '🌱 intro',
     Board: () => (
         <div className={styles.root}>
-            <p className={styles.myStyle}>How to use e-commerce starter?</p>
-            <p className={styles.myParagraph}>
-                Welcome to Codux e-commerce starter. This template was built to help you create your
-                own store easily. Use Codux to design your website the way you want, and Wix
-                Headless e-commerce services to manage your store. Learn more about it here.{' '}
-            </p>
+            <div className={styles.container}>
+                <p className={styles.myStyle}>How to use e-commerce starter?</p>
+                <p className={styles.myParagraph}>
+                    Welcome to Codux e-commerce starter. This template was built to help you create
+                    your own store easily. Use Codux to design your website the way you want, and
+                    Wix Headless e-commerce services to manage your store. Learn more about it here.{' '}
+                </p>
+            </div>
         </div>
     ),
     isSnippet: false,
