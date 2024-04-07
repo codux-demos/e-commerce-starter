a2e9759a8637047f6d4af6d9852f838622f76640
ShireeWill <shiree@wix.com>
2024-04-07 14:23:19 +0300

    fixed themes

--- src/_codux/boards/documentation/vars/vars.board.tsx
+++ src/_codux/boards/documentation/vars/vars.board.tsx
@@ -9,7 +9,7 @@ export default createBoard({
         <div className={documentationStyles.root}>
             <p className={styles.Title}>Fonts</p>
             <div className={styles['fonts-layout']}>
-                <div>
+                <div className={styles.flex}>
                     <p className={styles.miniTitle}>------ Title styles -----</p>
                     <div className={styles['extra-large-thin-title']}>Extra large thin title</div>
                     <div className={styles['large-thin-title']}>Large thin title</div>
@@ -18,16 +18,12 @@ export default createBoard({
                     <div className={styles['medium-title']}>Medium title</div>
                     <div className={styles['extra-small-title']}>Extra small title</div>
                 </div>
-                <div>
+                <div className={styles.flex}>
                     <p className={styles.miniTitle}>------ Paragraph styles -----</p>
+                    <div className={styles['extra-large-text']}>Extra Large text</div>
                     <div className={styles['large-paragraph']}>Large paragraph</div>
                     <div className={styles['medium-paragraph-bold']}>Medium paragraph bold</div>
-                    <div className={styles['paragraph-font']}>Paragraph font</div>
-                    <div className={styles['small-paragraph-font']}>Small paragraph font</div>
-                </div>
-                <div>
-                    <p className={styles.miniTitle}>------ Running text styles -----</p>
-                    <div className={styles['extra-large-text']}>Large text</div>
+                    <div className={styles['small-text-font-bold']}>Small text font bold</div>
                     <div className={styles['small-text']}>Small text</div>
                     <div className={styles['extra-small-text']}>Extra small text</div>
                 </div>
