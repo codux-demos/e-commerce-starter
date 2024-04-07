732f3f03ad030fcaf41be5a444fad80a32da0fa0
ShireeWill <shiree@wix.com>
2024-04-07 18:22:20 +0300

    fix text

--- src/_codux/boards/intro/intro.board.tsx
+++ src/_codux/boards/intro/intro.board.tsx
@@ -1,17 +1,21 @@
 import { createBoard } from '@wixc3/react-board';
 import styles from './intro.module.scss';
 import Classnames from 'classnames';
+import CommonStyles_module from '../../../styles/common-styles.module.scss';
 
 export default createBoard({
     name: '🌱 intro',
     Board: () => (
         <div className={styles.root}>
             <div className={styles.container}>
-                <p className={styles.myStyle}>HOW TO USE E-COMMERCE STARTER?</p>
+                <p className={styles.myStyle}>
+                    HOW TO USE <br /> E-COMMERCE STARTER?
+                </p>
                 <p className={styles.myParagraph}>
-                    Welcome to Codux e-commerce starter. This template was built to help you create
-                    your own store easily. Use Codux to design your website the way you want, and
-                    Wix Headless e-commerce services to manage your store. Learn more about it here.{' '}
+                    Welcome to Codux e-commerce starter. <br /> This template was built to help you
+                    create your own store easily. Use Codux to design your website the way you want,
+                    and Wix Headless e-commerce services to manage your store.{' '}
+                    <button className={CommonStyles_module['intro-button']}>Learn more</button>
                 </p>
             </div>
         </div>
@@ -19,7 +23,7 @@ export default createBoard({
     isSnippet: false,
     environmentProps: {
         canvasMargin: { right: 0, left: 0, top: 1 },
-        windowWidth: 1024,
+        windowWidth: 2000,
         windowHeight: 768,
     },
 });
