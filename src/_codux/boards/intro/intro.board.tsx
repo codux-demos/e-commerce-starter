3c212fa4193a93f1a52a6cb4ad32b8a68cb1f3b7
Sagiv Dayan <sagiv.keeper@gmail.com>
2024-04-08 15:15:47 +0300

    font and spacing changes; media queries

--- src/_codux/boards/intro/intro.board.tsx
+++ src/_codux/boards/intro/intro.board.tsx
@@ -6,24 +6,18 @@ import CommonStyles_module from '../../../styles/common-styles.module.scss';
 export default createBoard({
     name: '🌱 intro',
     Board: () => (
-        <div className={styles.root}>
-            <div className={styles.container}>
-                <p className={styles.myStyle}>
-                    HOW TO USE <br /> E-COMMERCE STARTER?
-                </p>
-                <p className={styles.myParagraph}>
-                    Welcome to Codux e-commerce starter. <br /> This template was built to help you
-                    create your own store easily. Use Codux to design your website the way you want,
-                    and Wix Headless e-commerce services to manage your store.{' '}
-                    <button className={CommonStyles_module['intro-button']}>Learn more</button>
-                </p>
-            </div>
+        <div className={styles.container}>
+            <p className={styles.myStyle}>
+                HOW TO USE <br /> E-COMMERCE STARTER?
+            </p>
+            <p className={styles.myParagraph}>
+                Welcome to Codux e-commerce starter. <br /> This template was built to help you
+                create your own store easily. Use Codux to design your website the way you want, and
+                Wix Headless e-commerce services to manage your store.
+                <button className={CommonStyles_module['intro-button']}>Learn more</button>
+            </p>
         </div>
     ),
     isSnippet: false,
-    environmentProps: {
-        canvasMargin: { right: 0, left: 0, top: 1 },
-        windowWidth: 2000,
-        windowHeight: 768,
-    },
+    environmentProps: { windowWidth: 1024, windowHeight: 768 },
 });
