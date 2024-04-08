aa0087a6735cdcf73931e6c5674060765f920a6b
ShireeWill <101344033+ShireeWill@users.noreply.github.com>
2024-04-08 16:01:09 +0300

    feat(boards): design intro board (#58)
    Co-authored-by: Sagiv Dayan <sagiv.keeper@gmail.com>

--- src/_codux/boards/intro/intro.board.tsx
+++ src/_codux/boards/intro/intro.board.tsx
@@ -1,23 +1,23 @@
 import { createBoard } from '@wixc3/react-board';
 import styles from './intro.module.scss';
 import Classnames from 'classnames';
+import CommonStyles_module from '../../../styles/common-styles.module.scss';
 
 export default createBoard({
     name: '🌱 intro',
     Board: () => (
-        <div className={styles.root}>
-            <p className={styles.myStyle}>How to use e-commerce starter?</p>
+        <div className={styles.container}>
+            <p className={styles.myStyle}>
+                HOW TO USE <br /> E-COMMERCE STARTER?
+            </p>
             <p className={styles.myParagraph}>
-                Welcome to Codux e-commerce starter. This template was built to help you create your
-                own store easily. Use Codux to design your website the way you want, and Wix
-                Headless e-commerce services to manage your store. Learn more about it here.{' '}
+                Welcome to Codux e-commerce starter. <br /> This template was built to help you
+                create your own store easily. Use Codux to design your website the way you want, and
+                Wix Headless e-commerce services to manage your store.
+                <button className={CommonStyles_module['intro-button']}>Learn more</button>
             </p>
         </div>
     ),
     isSnippet: false,
-    environmentProps: {
-        canvasMargin: { right: 0, left: 0, top: 1 },
-        windowWidth: 1024,
-        windowHeight: 768,
-    },
+    environmentProps: { windowWidth: 1024, windowHeight: 768 },
 });
