8ac2b0911d59cd48a46a5399000896c476442651
matan georgi <matang@wix.com>
2024-07-30 18:12:26 +0300

    adds new strips and move css to the right place

--- src/_codux/boards/documentation/common-styles.board.tsx
+++ src/_codux/boards/documentation/common-styles.board.tsx
@@ -5,6 +5,7 @@ import styles from './vars.module.scss';
 import cx from 'classnames';
 import { ProductCard } from '/src/components/product-card/product-card';
 import HeroImage03Jpg from '../../../assets/img/[1024_640]_heroImage03.jpg';
+import BlueJpg from '../../fake-data/images/[502_310]_blue.jpg';
 
 export default createBoard({
     name: 'UI Kit',
@@ -47,15 +48,20 @@ export default createBoard({
             </div>
             <div className={styles['large-thin-title']}>Page Strips</div>
             <div className={boardStyle.row}>
-                <div className={boardStyle.strip}>
-                    <div className={boardStyle.section}>
+                <div className={commonStyles.dividedStrip}>
+                    <div className={commonStyles.section}>
                         <div className={styles['medium-paragraph']}>
                             Great Deals Are <br /> Just a Click Away
                         </div>
                     </div>
                     <div>
-                        <div className={boardStyle.imageSection}>
-                            <div className={boardStyle.floatingContent}>
+                        <div className={commonStyles.imageSection}>
+                            <div
+                                className={cx(
+                                    commonStyles.floatingContent,
+                                    commonStyles.shopNowContent,
+                                )}
+                            >
                                 <div className={styles['small-text']}>
                                     Explore our latest deals 
                                     <br />
@@ -68,6 +74,17 @@ export default createBoard({
                             <img src={HeroImage03Jpg} alt="" />
                         </div>
                     </div>
+                </div>
+            </div>
+            <div className={boardStyle.row}>
+                <div className={commonStyles.strip}>
+                    <div className={cx(commonStyles.floatingContent, commonStyles.exploreContent)}>
+                        <div className={styles['extra-large-thin-title']}>
+                            Explore What <br /> Lies Beyond
+                        </div>
+                        <button className={commonStyles.secondaryButton}>Contact Us</button>
+                    </div>
+                    <img src={BlueJpg} className={commonStyles.coverImage} />
                 </div>
             </div>
 
