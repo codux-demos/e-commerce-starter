62bfb66b1358d7d6ca09b0b7f0fc361e0218ca3a
matan georgi <matang@wix.com>
2024-07-30 18:25:19 +0300

    move page strips to dedicated board

--- src/_codux/boards/documentation/common-styles.board.tsx
+++ src/_codux/boards/documentation/common-styles.board.tsx
@@ -4,8 +4,6 @@ import boardStyle from './documentation.module.scss';
 import styles from './vars.module.scss';
 import cx from 'classnames';
 import { ProductCard } from '/src/components/product-card/product-card';
-import HeroImage03Jpg from '../../../assets/img/[1024_640]_heroImage03.jpg';
-import BlueJpg from '../../fake-data/images/[502_310]_blue.jpg';
 
 export default createBoard({
     name: 'UI Kit',
@@ -45,47 +43,6 @@ export default createBoard({
                     style={{ width: '300px' }}
                     imageUrl="https://wixmp-b7f7090100b13623109851bc.wixmp.com/layouters-starters/img_02.jpg"
                 />
-            </div>
-            <div className={styles['large-thin-title']}>Page Strips</div>
-            <div className={boardStyle.row}>
-                <div className={commonStyles.dividedStrip}>
-                    <div className={commonStyles.section}>
-                        <div className={styles['medium-paragraph']}>
-                            Great Deals Are <br /> Just a Click Away
-                        </div>
-                    </div>
-                    <div>
-                        <div className={commonStyles.imageSection}>
-                            <div
-                                className={cx(
-                                    commonStyles.floatingContent,
-                                    commonStyles.shopNowContent,
-                                )}
-                            >
-                                <div className={styles['small-text']}>
-                                    Explore our latest deals 
-                                    <br />
-                                    and products
-                                </div>
-                                <div>
-                                    <button className={commonStyles.primaryButton}>Shop Now</button>
-                                </div>
-                            </div>
-                            <img src={HeroImage03Jpg} alt="" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className={boardStyle.row}>
-                <div className={commonStyles.strip}>
-                    <div className={cx(commonStyles.floatingContent, commonStyles.exploreContent)}>
-                        <div className={styles['extra-large-thin-title']}>
-                            Explore What <br /> Lies Beyond
-                        </div>
-                        <button className={commonStyles.secondaryButton}>Contact Us</button>
-                    </div>
-                    <img src={BlueJpg} className={commonStyles.coverImage} />
-                </div>
             </div>
 
             <div>
